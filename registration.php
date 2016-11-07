163724f2982b2f9edf3f33374006440667d6992f
Alex Wang <selltrends@gmail.com>
2016-11-07 14:00:22 +0800

    init magento 2 slider widget module

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2017 Atopt Limited. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Atopt_Slider',
+    __DIR__
+);
